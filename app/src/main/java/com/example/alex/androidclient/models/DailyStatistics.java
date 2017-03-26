5a96358c8cb5626d19b5e6e73d7758c5b633e302
Jaguarhl <dek.alpha@gmail.com>
2017-03-26 10:33:57 +0300

    - adding Parcelable to DailyStats

--- app/src/main/java/com/example/alex/androidclient/models/DailyStatistics.java
+++ app/src/main/java/com/example/alex/androidclient/models/DailyStatistics.java
@@ -1,5 +1,8 @@
 package com.example.alex.androidclient.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 import java.util.List;
 
@@ -7,7 +10,7 @@ import java.util.List;
  * Created by Jag on 22.03.2017.
  */
 
-public class DailyStatistics {
+public class DailyStatistics implements Parcelable {
     private int siteID;
     private Date date;
     private List<PersonStats> statsList;
@@ -44,4 +47,30 @@ public class DailyStatistics {
     public void setStatsList(List<PersonStats> statsList) {
         this.statsList = statsList;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(siteID);
+    }
+
+    protected DailyStatistics(Parcel in) {
+        siteID = in.readInt();
+    }
+
+    public static final Creator<DailyStatistics> CREATOR = new Creator<DailyStatistics>() {
+        @Override
+        public DailyStatistics createFromParcel(Parcel in) {
+            return new DailyStatistics(in);
+        }
+
+        @Override
+        public DailyStatistics[] newArray(int size) {
+            return new DailyStatistics[size];
+        }
+    };
 }
